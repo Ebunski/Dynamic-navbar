03513b09a19f522f010fdc83d6a4d1ff96da9a61
Oyebola Ebunoluwa <oyebolaebun07@gmail.com>
2022-06-28 11:46:59 +0100

    created navbar showing links on toggle(static link container)
    git status

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,47 @@
-import React, { useState, useRef, useEffect } from 'react'
-import { FaBars, FaTwitter } from 'react-icons/fa'
-import { links, social } from './data'
-import logo from './logo.svg'
+import React, { useState, useRef, useEffect } from "react";
+import { FaBars, FaTwitter } from "react-icons/fa";
+import { links, social } from "./data";
+import logo from "./logo.svg";
 
 const Navbar = () => {
-  return <h4>navbar</h4>
-}
+  const [show, setShow] = useState(false);
+  const navRef = useRef();
 
-export default Navbar
+  // useEffect(() => console.log(navRef.current.className = ), []);
+
+  const pages = links.map((x) => (
+    <a key={x.id} href={x.url}>
+      {x.text}
+    </a>
+  ));
+
+  const socialLinks = social.map((x) => (
+    <a key={x.id} href={x.url}>
+      {x.icon}
+    </a>
+  ));
+
+  ////------html page--------////
+
+  return (
+    <nav className="nav-center">
+      <header className="nav-header">
+        <img className="logo" src={logo} alt="coding-addict" />
+        <button className="nav-toggle" onClick={() => setShow(!show)}>
+          <FaBars />
+        </button>
+      </header>
+
+      <div
+        ref={navRef}
+        className={`${show && "show-container"}  ${"links-container"}`}
+      >
+        <ul className="links">{pages}</ul>
+      </div>
+
+      <ul className="social-icons"> {socialLinks}</ul>
+    </nav>
+  );
+};
+
+export default Navbar;
